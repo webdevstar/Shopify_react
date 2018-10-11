6e0419f58122e9d21f5356082d2215b78de9b449
woorihyon <38218870+woorihyon@users.noreply.github.com>
2018-10-11 13:42:07 +0800

    Add files via upload

--- src/reducers/cart_item.js
+++ src/reducers/cart_item.js
@@ -16,6 +16,7 @@ const chart = (state = initialState, action) => {
       {
         const newArray = state.cart_items.products.slice(0);
         const indexToRemove = newArray.findIndex((e) => e.id === action.payload)
+		state.cart_items.quantity = state.cart_items.quantity - newArray[indexToRemove].quantity
         if (indexToRemove > -1) {
           newArray.splice(indexToRemove, 1);
         }
